16942d1ebc5b54edd21498a47ea84fc59a6b25a5
Evgenii Tikhomirov <zhekaxa@ya.ru>
2020-07-24 11:25:10 +0300

    add feature text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker-compose
+          Feature text
         </p>
         <a
           className="App-link"
